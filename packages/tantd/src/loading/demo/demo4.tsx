82329b630dd7d2183c0c5bd45dc23a9aaf78cc8c
yunfang <yunye111@gmail.com>
2021-02-22 14:08:04 +0800

    fix:loading demo优化

--- packages/tantd/src/loading/demo/demo4.tsx
+++ packages/tantd/src/loading/demo/demo4.tsx
@@ -3,38 +3,20 @@ import { Loading } from 'tantd';
 
 //提供多种动态样式
 export default () => {
-  let arr = new Array(28).fill(1);
+  let arrType=[
+    'square-spin','triangle-skew-spin','ball-clip-rotate','ball-clip-rotate-pulse','ball-clip-rotate-multiple',
+    'ball-rotate','ball-pulse','ball-beat','ball-pulse-sync','ball-spin-fade-loader',
+    'line-spin-fade-loader','ball-scale','ball-scale-multiple','ball-scale-ripple','ball-scale-ripple-multiple',
+    'cube-transition','ball-zig-zag','ball-zig-zag-deflect','ball-pulse-rise','ball-triangle-path',
+    'semi-circle-spin','line-scale','line-scale-pulse-out','line-scale-pulse-out-rapid','line-scale-party',
+    'pacman','ball-grid-beat','ball-grid-pulse'
+  ];
   return (
     <>
       <div style={{ height: '420px' }}>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="square-spin" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="triangle-skew-spin" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-clip-rotate" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-clip-rotate-pulse" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-clip-rotate-multiple" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-rotate" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-pulse" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-beat" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-pulse-sync" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-spin-fade-loader" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="line-spin-fade-loader" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-scale" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-scale-multiple" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-scale-ripple" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-scale-ripple-multiple" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="cube-transition" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-zig-zag" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-zig-zag-deflect" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-pulse-rise" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-triangle-path" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="semi-circle-spin" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="line-scale" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="line-scale-pulse-out" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="line-scale-pulse-out-rapid" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="line-scale-party" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="pacman" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-grid-beat" /></div>
-        <div style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType="ball-grid-pulse" /></div>
+        {arrType.map((item,index)=>{
+          return <div key={index} style={{ width: '110px', height: '110px', float: 'left' }}><Loading spinType={item} /></div>
+        })}
       </div>
     </>
   );
